7cad509d6bdd4489e52c5c8d6684e62eb4a08dc4
withsahil <sahil01.awasthi@gmail.com>
2019-04-14 12:25:54 +0530

    Files added

---
+++ classes/Mail.php
@@ -0,0 +1,22 @@
+<?php
+require_once('PHPMailer/PHPMailerAutoload.php');
+class Mail {
+        public static function sendMail($subject, $body, $address) {
+                $mail = new PHPMailer();
+                $mail->isSMTP();
+                $mail->SMTPAuth = true;
+                $mail->SMTPSecure = 'ssl';
+                $mail->Host = 'smtp.gmail.com';
+                $mail->Port = '465';
+                $mail->isHTML();
+                $mail->Username = 'econnect.smsb@gmail.com';
+                $mail->Password = '#Sahil!=123';
+                $mail->SetFrom('no-reply@econnect.org');
+                $mail->Subject = $subject;
+                $mail->Body = $body;
+                $mail->AddAddress($address);
+
+                $mail->Send();
+        }
+}
+?>
